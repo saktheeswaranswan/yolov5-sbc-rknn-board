79ad986bc4589758067b400036e04f22cdde60e8
15094064799 <w1053904672@163.com>
2021-01-28 16:58:17 +0800

    优化代码逻辑

--- rknn_detect_yolov5.py
+++ rknn_detect_yolov5.py
@@ -18,7 +18,7 @@ def get_new_size(img, scale):
     return tuple(map(int, np.array(img.shape[:2][::-1]) * scale))
 
 
-class Size:
+class AutoScale:
     def __init__(self, img, max_w, max_h):
         self._src_img = img
         self.scale = get_max_scale(img, max_w, max_h)
@@ -40,11 +40,11 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
-def filter_boxes(boxes, box_confidences, box_class_probs):
+def filter_boxes(boxes, box_confidences, box_class_probs, conf_thres):
     box_scores = box_confidences * box_class_probs  # 条件概率， 在该cell存在物体的概率的基础上是某个类别的概率
     box_classes = np.argmax(box_scores, axis=-1)  # 找出概率最大的类别索引
     box_class_scores = np.max(box_scores, axis=-1)  # 最大类别对应的概率值
-    pos = np.where(box_class_scores >= OBJ_THRESH)  # 找出概率大于阈值的item
+    pos = np.where(box_class_scores >= conf_thres)  # 找出概率大于阈值的item
     # pos = box_class_scores >= OBJ_THRESH  # 找出概率大于阈值的item
     boxes = boxes[pos]
     classes = box_classes[pos]
@@ -52,7 +52,7 @@ def filter_boxes(boxes, box_confidences, box_class_probs):
     return boxes, classes, scores
 
 
-def nms_boxes(boxes, scores):
+def nms_boxes(boxes, scores, iou_thres):
     x = boxes[:, 0]
     y = boxes[:, 1]
     w = boxes[:, 2]
@@ -76,18 +76,18 @@ def nms_boxes(boxes, scores):
         inter = w1 * h1
 
         ovr = inter / (areas[i] + areas[order[1:]] - inter)
-        inds = np.where(ovr <= NMS_THRESH)[0]
+        inds = np.where(ovr <= iou_thres)[0]
         order = order[inds + 1]
     keep = np.array(keep)
     return keep
 
 
 def letterbox(img, new_wh=(416, 416), color=(114, 114, 114)):
-    s = Size(img, *new_wh)
-    new_img = s.new_img
+    a = AutoScale(img, *new_wh)
+    new_img = a.new_img
     h, w = new_img.shape[:2]
     new_img = cv2.copyMakeBorder(new_img, 0, new_wh[1] - h, 0, new_wh[0] - w, cv2.BORDER_CONSTANT, value=color)
-    return new_img, (new_wh[0] / s.scale, new_wh[1] / s.scale)
+    return new_img, (new_wh[0] / a.scale, new_wh[1] / a.scale)
 
 
 def load_model0(model_path, npu_id):
@@ -111,16 +111,14 @@ def load_model0(model_path, npu_id):
 
 
 def load_rknn_model(PATH):
-    # Create RKNN object
     rknn = RKNN()
-    # Load tensorflow model
     print('--> Loading model')
     ret = rknn.load_rknn(PATH)
     if ret != 0:
         print('load rknn model failed')
         exit(ret)
     print('done')
-    ret = rknn.init_runtime(device_id='TS018083200400178', rknn2precompile=True)
+    ret = rknn.init_runtime()
     if ret != 0:
         print('Init runtime environment failed')
         exit(ret)
@@ -128,88 +126,116 @@ def load_rknn_model(PATH):
     return rknn
 
 
-RKNN_MODEL_PATH = r"xxx.rknn"
-SIZE = (416, 416)
-CLASSES = ("classA", "classB")
-OBJ_THRESH = 0.4
-NMS_THRESH = 0.5
-MASKS = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
-ANCHORS = [[10, 13], [16, 30], [33, 23], [30, 61], [62, 45], [59, 119], [116, 90], [156, 198], [373, 326]]
+class RKNNDetector:
+    def __init__(self, model, wh, masks, anchors, classes):
+        self.wh = wh
+        self._masks = masks
+        self._anchors = anchors
+        self.classes = classes
+        if isinstance(model, str):
+            model = load_rknn_model(model)
+        self._rknn = model
 
+    def _predict(self, img_src, _img, gain, conf_thres=0.4, iou_thres=0.45):
+        src_h, src_w = img_src.shape[:2]
+        _img = cv2.cvtColor(_img, cv2.COLOR_BGR2RGB)
+        t0 = time.time()
+        pred_onx = self._rknn.inference(inputs=[_img])
+        print("inference time:\t", time.time() - t0)
+        boxes, classes, scores = [], [], []
+        for t in range(3):
+            input0_data = sigmoid(pred_onx[t][0])
+            input0_data = np.transpose(input0_data, (1, 2, 0, 3))
+            grid_h, grid_w, channel_n, predict_n = input0_data.shape
+            anchors = [self._anchors[i] for i in self._masks[t]]
+            box_confidence = input0_data[..., 4]
+            box_confidence = np.expand_dims(box_confidence, axis=-1)
+            box_class_probs = input0_data[..., 5:]
+            box_xy = input0_data[..., :2]
+            box_wh = input0_data[..., 2:4]
+            col = np.tile(np.arange(0, grid_w), grid_h).reshape(-1, grid_w)
+            row = np.tile(np.arange(0, grid_h).reshape(-1, 1), grid_w)
+            col = col.reshape((grid_h, grid_w, 1, 1)).repeat(3, axis=-2)
+            row = row.reshape((grid_h, grid_w, 1, 1)).repeat(3, axis=-2)
+            grid = np.concatenate((col, row), axis=-1)
+            box_xy = box_xy * 2 - 0.5 + grid
+            box_wh = (box_wh * 2) ** 2 * anchors
+            box_xy /= (grid_w, grid_h)  # 计算原尺寸的中心
+            box_wh /= self.wh  # 计算原尺寸的宽高
+            box_xy -= (box_wh / 2.)  # 计算原尺寸的中心
+            box = np.concatenate((box_xy, box_wh), axis=-1)
+            res = filter_boxes(box, box_confidence, box_class_probs, conf_thres)
+            boxes.append(res[0])
+            classes.append(res[1])
+            scores.append(res[2])
+        boxes, classes, scores = np.concatenate(boxes), np.concatenate(classes), np.concatenate(scores)
+        nboxes, nclasses, nscores = [], [], []
+        for c in set(classes):
+            inds = np.where(classes == c)
+            b = boxes[inds]
+            c = classes[inds]
+            s = scores[inds]
+            keep = nms_boxes(b, s, iou_thres)
+            nboxes.append(b[keep])
+            nclasses.append(c[keep])
+            nscores.append(s[keep])
+        if len(nboxes) < 1:
+            return [], []
+        boxes = np.concatenate(nboxes)
+        classes = np.concatenate(nclasses)
+        scores = np.concatenate(nscores)
+        label_list = []
+        box_list = []
+        for (x, y, w, h), score, cl in zip(boxes, scores, classes):
+            x *= gain[0]
+            y *= gain[1]
+            w *= gain[0]
+            h *= gain[1]
+            top = max(0, np.floor(x).astype(int))
+            left = max(0, np.floor(y).astype(int))
+            right = min(src_w, np.floor(x + w + 0.5).astype(int))
+            bottom = min(src_h, np.floor(y + h + 0.5).astype(int))
+            label_list.append(self.classes[cl])
+            box_list.append((top, left, right, bottom))
+        return label_list, box_list
 
-def predict(img_src, rknn):
-    src_h, src_w = img_src.shape[:2]
-    img, gain = letterbox(img_src, SIZE)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    t0 = time.time()
-    print("img shape               \t:", img.shape)
-    pred_onx = rknn.inference(inputs=[img])
-    print("time:                    \t", time.time() - t0)
-    boxes, classes, scores = [], [], []
-    for t in range(3):
-        input0_data = sigmoid(pred_onx[t][0])
-        input0_data = np.transpose(input0_data, (1, 2, 0, 3))
-        grid_h, grid_w, channel_n, predict_n = input0_data.shape
-        anchors = [ANCHORS[i] for i in MASKS[t]]
-        box_confidence = input0_data[..., 4]
-        box_confidence = np.expand_dims(box_confidence, axis=-1)
-        box_class_probs = input0_data[..., 5:]
-        box_xy = input0_data[..., :2]
-        box_wh = input0_data[..., 2:4]
-        col = np.tile(np.arange(0, grid_w), grid_h).reshape(-1, grid_w)
-        row = np.tile(np.arange(0, grid_h).reshape(-1, 1), grid_w)
-        col = col.reshape((grid_h, grid_w, 1, 1)).repeat(3, axis=-2)
-        row = row.reshape((grid_h, grid_w, 1, 1)).repeat(3, axis=-2)
-        grid = np.concatenate((col, row), axis=-1)
-        box_xy = box_xy * 2 - 0.5 + grid
-        box_wh = (box_wh * 2) ** 2 * anchors
-        box_xy /= (grid_w, grid_h)  # 计算原尺寸的中心
-        box_wh /= SIZE  # 计算原尺寸的宽高
-        box_xy -= (box_wh / 2.)  # 计算原尺寸的中心
-        box = np.concatenate((box_xy, box_wh), axis=-1)
-        res = filter_boxes(box, box_confidence, box_class_probs)
-        boxes.append(res[0])
-        classes.append(res[1])
-        scores.append(res[2])
-    boxes, classes, scores = np.concatenate(boxes), np.concatenate(classes), np.concatenate(scores)
-    nboxes, nclasses, nscores = [], [], []
-    for c in set(classes):
-        inds = np.where(classes == c)
-        b = boxes[inds]
-        c = classes[inds]
-        s = scores[inds]
-        keep = nms_boxes(b, s)
-        nboxes.append(b[keep])
-        nclasses.append(c[keep])
-        nscores.append(s[keep])
-    if len(nboxes) < 1:
-        return [], []
-    boxes = np.concatenate(nboxes)
-    classes = np.concatenate(nclasses)
-    scores = np.concatenate(nscores)
-    label_list = []
-    box_list = []
-    for (x, y, w, h), score, cl in zip(boxes, scores, classes):
-        x *= gain[0]
-        y *= gain[1]
-        w *= gain[0]
-        h *= gain[1]
-        top = max(0, np.floor(x).astype(int))
-        left = max(0, np.floor(y).astype(int))
-        right = min(src_w, np.floor(x + w + 0.5).astype(int))
-        bottom = min(src_h, np.floor(y + h + 0.5).astype(int))
-        label_list.append(CLASSES[cl])
-        box_list.append((top, left, right, bottom))
-    return label_list, np.array(box_list)
+    def predict_resize(self, img_src, conf_thres=0.4, iou_thres=0.45):
+        """
+        预测一张图片，预处理使用resize
+        return: labels,boxes
+        """
+        _img = cv2.resize(img_src, self.wh)
+        gain = img_src.shape[:2][::-1]
+        return self._predict(img_src, _img, gain, conf_thres, iou_thres, )
+
+    def predict(self, img_src, conf_thres=0.4, iou_thres=0.45):
+        """
+        预测一张图片，预处理保持宽高比
+        return: labels,boxes
+        """
+        _img, gain = letterbox(img_src, self.wh)
+        return self._predict(img_src, _img, gain, conf_thres, iou_thres)
+
+    def close(self):
+        self._rknn.release()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def __del__(self):
+        self.close()
 
 
 if __name__ == '__main__':
     RKNN_MODEL_PATH = r"xxx.rknn"
     SIZE = (640, 640)
     CLASSES = ("classA", "classB")
-    OBJ_THRESH = 0.4
-    NMS_THRESH = 0.5
-    rknn = load_rknn_model(RKNN_MODEL_PATH)
-    predict.__defaults__ = (None, rknn)
+    MASKS = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+    ANCHORS = [[10, 13], [16, 30], [33, 23], [30, 61], [62, 45], [59, 119], [116, 90], [156, 198], [373, 326]]
+    model = load_rknn_model(RKNN_MODEL_PATH)
+    detector = RKNNDetector(model, SIZE, MASKS, ANCHORS, CLASSES)
     img = cv2.imread("xxxx.png")
-    res = predict(img)
+    detector.predict(img)
